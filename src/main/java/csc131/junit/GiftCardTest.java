1a580835ba2879c06de3d5bff2875c2a640cfa4c
Andy Cha <58677180+annoyingnewb@users.noreply.github.com>
2021-03-11 21:09:14 -0800

    Add files via upload
    finished test

--- src/main/java/csc131/junit/GiftCardTest.java
+++ src/main/java/csc131/junit/GiftCardTest.java
@@ -1,16 +1,15 @@
 package csc131.junit;
 
-//import static org.junit.jupiter.api.Assertions.*;
-
-//import org.junit.jupiter.api.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.Test; 
 import org.junit.rules.ExpectedException;
 
-class GiftCardTest {
-
+public class GiftCardTest {	
+	
 	@Test
-	public void testGetIssuingStore() {
+	public void getIssuingStore() {
 		double     balance;
 		GiftCard   card;
 		int       issuingStore;
@@ -32,18 +31,59 @@ class GiftCardTest {
 		card = new GiftCard(issuingStore, balance);
 		assertEquals("getBalance()", balance, card.getBalance(), 0.001);
 	}
-	
+
+	@Test
 	public void deduct_RemainingBalance() {
 		double     balance;
 		GiftCard   card;
 		int       issuingStore;
-		String amount = "";
+		String amount;
 		
 		issuingStore = 1337;
 		balance      = 100.00;
 		card = new GiftCard(issuingStore, balance);
-		assertEquals("deduct(20.00)", amount, card.deduct(20.00));
+		amount = "Remaining Balance: " + String.format("%6.2f", 90.00);
+		assertEquals("deduct(10.00)", amount, card.deduct(10.00));
+		}
+	
+	@Test
+	public void deduct_amountDue() {
+		double     balance;
+		GiftCard   card;
+		int       issuingStore;
+		String amount = "-10.00";
+		
+		issuingStore = 1337;
+		balance      = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		amount = "Amount Due: " + String.format("%6.2f", 10.00);
+		assertEquals("deduct(110.00)", amount, card.deduct(110.00));
+		
 	}
 	
+	@Test
+	public void deduct_invalidTransaction() {
+		double     balance;
+		GiftCard   card;
+		int       issuingStore;
+		String amount = "Invalid";
+		
+		issuingStore = 1337;
+		balance      = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		amount = "Invalid Transaction";
+		assertEquals("deduct(-20.00)", amount, card.deduct(-20.00));
+		
+	}
 
+	@Test 
+	public void constructor_IncorrectID_Low(){
+		assertThrows(IllegalArgumentException.class, () -> {new GiftCard(-1,100.00);});
+	}
+	
+	@Test
+	public void constructor_IncorrectID_High(){
+		assertThrows(IllegalArgumentException.class, () -> {new GiftCard(1,-100.00);});
+	}
+	
 }
